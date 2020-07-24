6c4193e957298598fd9b549270149483ebf96f00
Cameron Turner <cammy_smfc@live.com>
2020-07-24 15:30:21 +0100

    adding first html template

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 import functions
 
 app = Flask(__name__)
 
+filter = {}
+filter["title"] = ""
+filter["dateTo"] = ""
+filter["dateFrom"] = ""
+
+data = functions.get_data(filter)
+
 
 @app.route('/')
-def render():
-    filter = {}
-    filter["title"] = ""
-    filter["dateTo"] = ""
-    filter["dateFrom"] = ""
-
-    data = functions.get_data(filter)
+def home():
     print(data["youtube_count"])
-    
-    return "Result " + str(data["youtube_count"])
+
+    return render_template("home.html", data=data)
