4b7acb24a5c756e20ec57f7c080cc2ec76d81c0b
Cameron Turner <cammy_smfc@live.com>
2020-07-24 18:16:08 +0100

    adding bootstrap for UI

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask_minify import minify
+from flask_bootstrap import Bootstrap
 import functions
 
 app = Flask(__name__)
+Bootstrap(app)
+
+minify(app=app, html=True, js=True, cssless=True)
 
 
 def get_info():
