3782b884c41063a5c8216c4a0d0dc56830a66330
cturner8 <cammy_smfc@live.com>
2020-09-26 19:44:03 +0100

    initial auth flow, attempt to accept auth header

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials, auth
+
 import functions
 from flask import jsonify
 
 
+cred = credentials.Certificate("./serviceAccount.json")
+firebase_admin.initialize_app(cred)
+
+
 def search_data(request):
+    if "Authorization" not in request.headers:
+        return ({'error': 'no authorization token'}, 500)
+
+    decoded_token = auth.verify_id_token(
+        request.headers["Authorization"])
+    uid = decoded_token['uid']
+    print(uid)
+
     response_body = functions.search_data(request)
 
     # Set CORS headers for the preflight request
@@ -12,7 +27,7 @@ def search_data(request):
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             'Access-Control-Max-Age': '3600'
         }
 
