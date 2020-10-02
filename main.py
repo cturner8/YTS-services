57f406ac0a0d789042c5b0617f562c05b58e58b0
cturner8 <cammy_smfc@live.com>
2020-10-02 19:02:30 +0100

    added some error handling

--- main.py
+++ main.py
@@ -10,16 +10,6 @@ firebase_admin.initialize_app(cred)
 
 
 def search_data(request):
-    if "Authorization" not in request.headers:
-        return ({'error': 'no authorization token'}, 500)
-
-    decoded_token = auth.verify_id_token(
-        request.headers["Authorization"])
-    uid = decoded_token['uid']
-    print(uid)
-
-    response_body = functions.search_data(request)
-
     # Set CORS headers for the preflight request
     if request.method == 'OPTIONS':
         # Allows POST requests from any origin with the Content-Type
@@ -32,6 +22,23 @@ def search_data(request):
         }
 
         return ('', 204, headers)
+
+    if "Authorization" not in request.headers:
+        return ({'error': 'no authorization token'}, 500)
+
+    try:
+        decoded_token = auth.verify_id_token(
+            request.headers["Authorization"])
+    except:
+        return ({'error': 'could not verify auth token'}, 500)
+
+    uid = decoded_token['uid']
+    if uid is None:
+        return ({'error': 'could not verify auth token contents'}, 500)
+
+    print(uid)
+
+    response_body = functions.search_data(request)
 
     response = jsonify(response_body)
     headers = {
